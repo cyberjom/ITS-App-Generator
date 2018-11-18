90f9c5dcf48bb817d128ea89b2db14a5c389cea9
Pinit Asavanuchit <pinit.asavanuchit@icloud.com>
2018-11-18 23:22:24 +0700

    table work now

--- lib/ias.rb
+++ lib/ias.rb
@@ -1,9 +1,28 @@
 require "ias/version"
 # include ActionView::Helpers::TagHelper
+include ActionView::Helpers::UrlHelper
+
 
 module Ias
   class View
+    
+    def self.list(data, &block)
+      block.call
+      return table(data)
+    end
+    
+    def self.t
+
+    end
+    
     def self.table(data)
+      patch_row = data.limit_value-data.count
+      ap patch_row
+      path = "/"+data.model.to_s.pluralize.downcase
+      prevp = data.prev_page || 1
+      lastp = data.total_pages
+      nextp = data.next_page || lastp
+      
       attrs = data[0].attribute_names
       out = '<table class="table table-sm table-striped table-bordered table-hover">'
         out << '<thead class="thead-light">'
@@ -26,9 +45,23 @@ module Ias
                 out << '</td>'     
               end
             out << '</tr>'
-          end        
+          end
+          ## Patching rows
+          # patch_row.times do
+          #   out << '<tr>'
+          #     attrs.each do |a|
+          #       out << '<td> </td>'
+          #     end
+          #   out << '</tr>'
+          # end
         out << '</tbody>'
       out << '</table>'
+      out << '<br>'
+      out << "#{link_to '<<', path} #{link_to '<', path+'?page='+prevp.to_s}"
+      out << "....."
+      out << "#{link_to '>', path+'?page='+nextp.to_s} #{link_to '>>', path+'?page='+lastp.to_s} "
+      # out << "#{request}"
+      
       return out
     end
   end
