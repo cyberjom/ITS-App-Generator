f6053883b70c8d5e9144d9d0ae185edc788bd2b0
Pinit Asavanuchit <pinit.asavanuchit@icloud.com>
2018-11-18 16:50:56 +0700

    table fin

--- lib/ias.rb
+++ lib/ias.rb
@@ -1,20 +1,36 @@
 require "ias/version"
+# include ActionView::Helpers::TagHelper
 
 module Ias
   class View
     def self.table(data)
-      out = '<table class="table">'
+      attrs = data[0].attribute_names
+      out = '<table class="table table-sm table-striped table-bordered table-hover">'
         out << '<thead class="thead-light">'
           out << '<tr>'
-            data[:headers].each do |h|
-              out << '<th scope="col">'+h.to_s+'</th>'
+            attrs.each do |a|
+              out << '<th scope="col">'+data.human_attribute_name(a.to_s)+'</th>'
             end
           out << '</tr>'
         out << '</thead>'
         out << '<tbody>'
+          data.each do |rec|
+            out << '<tr>'
+              attrs.each do |a|
+                out << '<td>'
+                if rec[a]
+                  out << rec[a].to_s
+                else
+                  out << ""
+                end
+                out << '</td>'     
+              end
+            out << '</tr>'
+          end        
         out << '</tbody>'
       out << '</table>'
       return out
     end
   end
 end
+
